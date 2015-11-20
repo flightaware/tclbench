cf04d1d4a81123e4237f1e2ae19782e8d359968d
Karl Lehenbauer <karl-gh@lehenbauer.com>
2015-11-20 22:04:04 +0000

    add comments

--- math/bench.tcl
+++ math/bench.tcl
@@ -1,9 +1,21 @@
+#
+# simple tcl math benchmark
+#
 
+# for more on calculating distance, bearing, midpoint, etc,
+# see http://www.movable-type.co.uk/scripts/latlong.html
 
+#
+# degrees_radians - convert degrees to radians
+#
 proc degrees_radians {degrees} {
 	return [expr {$degrees * 3.14159265358979323846 / 180.0}]
 }
 
+#
+# latlongs_to_distance - return the great circle distance
+#   between two points
+#
 proc latlongs_to_distance {lat1 lon1 lat2 lon2} {
 	set dLat [degrees_radians [expr {$lat2 - $lat1}]]
 	set dLon [degrees_radians [expr {$lon2 - $lon1}]]
@@ -16,6 +28,9 @@ proc latlongs_to_distance {lat1 lon1 lat2 lon2} {
 	return [expr {3963. * $c}]
 }
 
+#
+# test - run the latlongs_to_distance calculation a bunch of times
+#
 proc test {} {
 	for {set lon -180} {$lon < 180} {incr lon} {
 		for {set lat -90} {$lat < 90} {incr lat} {
